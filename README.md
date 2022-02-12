dc5a9e9cdac54bea7f2604eb79039ddcb026849c
1BObC <bob.cocksedge@gmail.com>
2022-02-12 14:26:01 +0000

    2:51:25 and ready to start tutorial same as chucksDemos

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Bob's demos of html, css and jscript 
 "bobs and chucks Demo files seperated"
 "Set up some trial p h p files and... they run!"
+"2:51:25 and ready to start tutorial 2:51:25"
--- index.php
+++ index.php
@@ -3,6 +3,22 @@
 <h1>First attempt with PHP</h1>
 <p>
 <?php
-    echo "Hallo to you!\n";
-    ?> 
+    echo "Hello to you!\n";
+?> 
 </p>
+
+<p><?php
+    $data = array("TWD ", "provides technical tutorials", " and they are good!");
+    echo $data[0];
+    echo $data[1];
+    echo $data[2];
+?></p>
+<p><?php
+    echo "  And these arrays say...", " - ", "Thank you TWD!";
+?></p>
+<p><?php 
+    echo "What is the answer of six times seven\n";
+    $answer = 6 * 7 ;
+    echo "If the answer is:  $answer, what was the question?\n";
+?></p>
+
