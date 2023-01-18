97b6d210d1c38a31f21be097c0c3585ec40b5450
1BObC <bob.cocksedge@gmail.com>
2023-01-18 21:46:39 +0000

    complete drop down list and text area

--- more.php
+++ more.php
@@ -20,13 +20,42 @@
         <p>Preferred Time:<br/>
         <input type= "radio" name="when" value= "am">AM<br>
         <input type= "radio" name="when" value= "pm" checked>PM</p>
-        <input type= "checkbox" name="class1" value="fcode12" checked>
+        <input type= "checkbox" name="class" value="fcode12" checked>
             fcode12 - HTML<br>
         <input type= "checkbox" name="class" value="pcode34">
             pcode34 - CSS<br>
         <input type= "checkbox" name="class">
         <!-- try this with and without a value -->
-            ncode56 - JavaScript<br></p>    
+            ncode56 - JavaScript<br></p> 
+            <br>
+        <p><label for="inp06">Which drink:
+        <select name="Fizz" id="inp06">
+            <option value="0">--Please select--</option>
+            <option value="1">Coke</option>
+            <option value="2">Pepsi</option>
+            <option value="3">Water</option>
+            <option value="4">Lemon</option>
+            <option value="5">Orange</option>
+        </select>
+        </p>
+        <!-- value can be text or integer -->
+        <!-- one item can be shown as selected eg ..."3" selected>Water... -->
+        <p><label for="inp07">Which snack:
+        <select name="Snack" id="inp07">
+            <option value="0">--Please select--</option>
+            <option value="1">Peanuts</option>
+            <option value="2">Burger</option>
+            <option value="3">Chips</option>
+            <option value="4">Apples</option>
+            <option value="5">Pears</option>
+        </select>
+        </p>
+        <br/>
+        <p><label for="inp08">Tell us all about yourself:<br/>
+            <textarea rows= "10" cols= "40" id= "inp08" name="about">
+                Well...
+        </textarea>
+        </p>   
         <input type = "submit"/>
     </form>   
     <pre> 
