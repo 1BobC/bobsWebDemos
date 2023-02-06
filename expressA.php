55818611f4b16abe697b8ce113643beb746980f3
1BObC <bob.cocksedge@gmail.com>
2023-02-06 17:28:26 +0000

    Orl Krect again and ready to continue with Model View Controller (MVC) from 05:00:43

--- expressA.php
+++ expressA.php
@@ -12,16 +12,25 @@
     <body>
     <!-- Continue with Data Security, Integrity, Validation 04:50:10 -->
     <!-- Do again from video -->
-
-    <h2>Validate incoming guessing game data</h2>
-    <form method= "get">
+    <!-- Done again from video! -->
+    <h2>Persisting form data across requests</h2>
+    <?php
+    $oldguess= isset($_POST["guess"]) ? $_POST["guess"] : " " ;
+    ?>
+    <p>Guessing game...</p>
+    <form method= "post">
     <p><label for = "guess">Input Guess</label>
     <input type= "text" name= "guess" id= "guess"
-        size= "40" /></p>
-     <input type = "submit"/>
+        size= "40" value= "<?= htmlentities($oldguess) ?>"/></p>
+        <!-- this line is trouble if left at "$oldguess" and if input has html characters, 
+        browser will interpret echo as html not intended input use htmlentities() instead -->
+    <input type = "submit"/>
     </form>
     
+    <?= $oldguess ?>
+    <?php echo($oldguess) ;  ?>
     
+    <h2>Validate incoming guessing game data</h2> 
     <p><?php
     if (! isset($_GET["guess"]) ) {
         echo("Missing guess parameter");
@@ -35,21 +44,13 @@
         echo("Your guess is too high");
     }else {
         echo("Congratulations your guess is correct!");
-    }
-    
+    }    
     ?></p>
-
-    
-    <pre> 
-        $_GET:
-    <?php
-        print_r($_GET);
-    ?> 
-    </pre>
-    "Whoa! 04:59:11 Data verification for a guessing game not finished (does'nt work!)"
+        "Whoa! 04:59:11 Data verification for a guessing game not finished (does'nt work!)"
     <h4>Whoa again! you have been trying to make isolated examples work, Dr C will put it together, 
         so watch and wait and learn!!!</h4>
 
+        
     <?php
     echo"<p>Page End</p>";
     ?>
